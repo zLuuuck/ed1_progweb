81110a5109877a9de15f74217ef66e33a335caae
zLuuuck <xx.lyck.nitgh.xx@gmail.com>
2025-04-14 19:09:10 -0300

    importação do footer

--- site/scripts/inicio.js
+++ site/scripts/inicio.js
@@ -1,17 +1,4 @@
-if (localStorage.getItem("token") == null) {
-    alert('Você precisa estar logado para acessar essa página!');
-    window.location.href = "/site/html/conta/login.html";
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
 }
-
-let userLogado = JSON.parse(localStorage.getItem("userLogado"));
-
-let user = document.querySelector("#logado");
-logado.innerHTML = 'Olá, ${userLogado.nome}';
-
-function logout() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("userLogado");
-    alert('Você saiu da sua conta!');
-    window.location.href = "/site/html/conta/login.html";
-}
-
