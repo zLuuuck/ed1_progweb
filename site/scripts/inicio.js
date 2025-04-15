6756eb0c65fc204dd5052843428cd93e1ff58923
zLuuuck <xx.lyck.nitgh.xx@gmail.com>
2025-04-15 12:47:34 -0300

    formulario de envio +/- feito, mas ta la

--- site/scripts/inicio.js
+++ site/scripts/inicio.js
@@ -2,3 +2,38 @@ function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 }
+
+document.getElementById('formulario').addEventListener('submit', function (e) {
+    e.preventDefault(); // impede redirecionamento
+
+    const form = e.target;
+    const data = new FormData(form);
+
+    fetch(form.action, {
+        method: "POST",
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            showSuccess("E-mail enviado com sucesso!");
+            form.reset(); // limpa o formulário
+        }
+    })
+});
+
+function showSuccess(message) {
+    const msgSuccess = document.querySelector("#msgSuccess");
+    msgSuccess.textContent = message;
+    msgSuccess.style.display = "block";
+    msgSuccess.style.color = "green";
+    document.querySelector("button[type='button']").disabled = true;
+}
+
+function showError(message) {
+    const msgError = document.querySelector("#msgError");
+    msgError.textContent = message;
+    msgError.style.display = "block";
+    msgError.style.color = "red";
+}
